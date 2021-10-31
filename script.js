46b0336f7443d16789365e6d1343351b9d8104b8
Parth Lad <parthlad3915@gmail.com>
2021-10-30 22:17:38 -0400

    Added Password Customization with checkboxes

--- script.js
+++ script.js
@@ -4,28 +4,47 @@ const generateBtn = document.getElementById('generateBtn');
 const generatedPassword =document.getElementById('generatedPassword');
 const clipboard = document.getElementById('copy-to-clip');
 
+const checked =() => {
+
+    const lowercase = document.getElementById('lowercase');
+    const uppercase = document.getElementById('uppercase');
+    const numbersCheckbox = document.getElementById('numbers');
+    const specialCharactersCheckbox = document.getElementById('specialCharacters');
+
+    const lowercaseCharacters="abcdefghijklmnopqrstuvwxyz"
+    const uppercaseCharacters = lowercaseCharacters.toUpperCase();
+    const numbers = "0123456789"
+    const specialCharacters = "@#$%^&*";
+    
+    let selectedCheckboxes = '';
+
+    lowercase.checked && (selectedCheckboxes +=lowercaseCharacters);
+    uppercase.checked && (selectedCheckboxes += uppercaseCharacters);
+    numbersCheckbox.checked && (selectedCheckboxes +=numbers);
+    specialCharactersCheckbox.checked && (selectedCheckboxes +=specialCharacters);
+
+    const passwordString = selectedCheckboxes ==='' ? lowercaseCharacters+uppercaseCharacters+numbers+specialCharacters : selectedCheckboxes;
+
+    return passwordString;
+}
 
 selectedPasswordLength.oninput =() => {
     passwordLength.textContent = selectedPasswordLength.value;
 }
 
 generateBtn.addEventListener('click',() => {
-    const characterString = "ABCDEFGHIJKLMNOPQRSTUVWXYZacdefghijklnopqrstuvwxyz0123456789";
     let randomPassword = '';
 
+    const passwordString = checked();
+
     for(let i=0; i< selectedPasswordLength.value; i++){
-        const password = characterString[Math.floor(Math.random()*characterString.length)];
+        const password = passwordString[Math.floor(Math.random()*passwordString.length)];
         randomPassword += password;
     }
-    
-    // console.log(randomPassword);
     generatedPassword.textContent = randomPassword;
     clipboard.style.visibility = 'visible';
 });
 
-clipboard.addEventListener('click',() => {
-
-        navigator.clipboard.writeText(generatedPassword.textContent);
-
-        alert(`Copied ${generatedPassword.textContent}`);
+clipboard.addEventListener('click',async () => {
+       await navigator.clipboard.writeText(generatedPassword.textContent).then(() => {alert(`Copied ${generatedPassword.textContent}`)}).catch((err)=> {alert(err.message)});
 })
